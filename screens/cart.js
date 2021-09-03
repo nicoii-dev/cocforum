6faf9643f7367916e3483f4bbd4178befe3823c7
Arjohn Bagalihog Ely <42762510+vankill35@users.noreply.github.com>
2021-09-03 08:12:32 +0800

    Update cart.js

--- screens/cart.js
+++ screens/cart.js
@@ -10,21 +10,20 @@ function Cart ({route, navigation}) {
 			{itemId: "35031861", name: "Better Homes & Gardens Leighton Twin Over Twin Wood Bunk Bed, Multiple Finishes", thumbnailImage: "https://i5.walmartimages.com/asr/4aedb609-4b61-4593-ad8a-cdc8c88696b1_1.3f505ce3d55db4745cf4c51d559994dc.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", qty: 1, color: "Green", salePrice: "199", checked: 0},
 			{itemId: "801099131", name: "LEGO Star Wars 2019 Advent Calendar 75245 Holiday Building Kit", thumbnailImage: "https://i5.walmartimages.com/asr/9a8ea1ab-311d-455c-bda8-ce15692a8185_3.208d48e0260f80891d32b351cb116a4b.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", qty: 1, color: "Blue", salePrice: "27.99", checked: 0},
 			{itemId: "42608079", name: "Little Tikes Cape Cottage Playhouse, Tan", thumbnailImage: "https://i5.walmartimages.com/asr/2654cd64-1471-44af-8b0c-1debaf598cb3_1.c30c481d1ac8fdd6aa041c0690d7214c.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", color: "Purple", qty: 1, salePrice: "129.99", checked: 0},
-			{itemId: "247714372", name: "HP 14\" Laptop, Intel Core i3-1005G1, 4GB SDRAM, 128GB SSD, Pale Gold, 14-DQ1038wm", thumbnailImage: "https://i5.walmartimages.com/asr/b442f6e7-c5e1-4387-9cd9-9205811d4980_1.82b94d1c11dd12a6697bc517219f331e.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF", qty: 1, color: "Black", salePrice: "269", checked: 0}
 	])
 
 	const [checkOutItems, setCheckOutItems] = useState([]);
 	const [showModal, setShowModal] = useState(false);
+	const [total, setTotal] = useState(1);
 
-
-	selectHandler = (index, value) => {
+	function selectHandler(index, value){
 		const newItems = [...cartItems]; // clone the array 
 		newItems[index]['checked'] = value == 1 ? 0 : 1; // set the new value 
 		setCartItems(newItems);// set new state
 	}
 	
 	
-	quantityHandler = (action, index) => {
+	function quantityHandler(action, index){
 		const newItems = [...cartItems]; // clone the array 
 		
 		let currentQty = newItems[index]['qty'];
@@ -38,7 +37,7 @@ function Cart ({route, navigation}) {
 		//this.setState({ cartItems: newItems }); // set new state
 	}
 
-	subtotalPrice = () => {
+	function subtotalPrice(){
 		const newItems = [...cartItems];
 		if(newItems){
 			return newItems.reduce((sum, item) => sum + (item.checked == 1 ? item.qty * item.salePrice : 0), 0 );
@@ -46,146 +45,34 @@ function Cart ({route, navigation}) {
 		return 0;
 	}
 
-	checkOut = () =>{
-		const newItems = [...cartItems]; // clone the array
-		let checkItems = [];
-		for (var i = 0; i < newItems.length; i++) {
-			if(newItems[i].checked == 1){
-				checkItems.push({"itemId":newItems[i].itemId, "name":newItems[i].name,"thumbnailImage":newItems[i].thumbnailImage,"color": newItems[i].color, "qty":newItems[i].qty, "salePrice":newItems[i].salePrice})
+	function checkOut(){
+		if(subtotalPrice() == 0){
+			alert("Please select item(s) to be paid");
+		}else{
+			const newItems = [...cartItems]; // clone the array
+			let checkItems = [];
+			for (var i = 0; i < newItems.length; i++) {
+				if(newItems[i].checked == 1){
+					checkItems.push({"itemId":newItems[i].itemId, "name":newItems[i].name,"thumbnailImage":newItems[i].thumbnailImage,"color": newItems[i].color, "qty":newItems[i].qty, "salePrice":newItems[i].salePrice})
+				}
 			}
+			setShowModal(true);
+			setCheckOutItems(checkItems);
 		}
-		console.log(checkItems);
-		setShowModal(true);
-		setCheckOutItems(checkItems);
 	}
 
-		const htmlContent = `
-            
-		<!DOCTYPE html>
-		<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Paypal Payment</title>
-				<!-- Latest compiled and minified CSS -->
-				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
-				<!-- jQuery library -->
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-				<!-- Latest compiled JavaScript -->
-				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-			</head>
-
-			<body>
-				<section class="pt-5 pb-5">
-				<div class="container">
-				<div class="row w-100">
-					<div class="col-lg-12 col-md-12 col-12">
-						<h3 class="display-5 mb-2 text-center">Shopping Cart</h3>
-						<p class="mb-5 text-center">
-							<i class="text-info font-weight-bold">3</i> items in your cart</p>
-						<table id="shoppingCart" class="table table-condensed table-responsive">
-							<thead>
-								<tr>
-									<th style="width:60%">Product</th>
-									<th style="width:12%">Price</th>
-									<th style="width:10%">Quantity</th>
-									<th style="width:16%"></th>
-								</tr>
-							</thead>
-							<tbody>
-							${
-								checkOutItems.map((item, i) => (
-									`
-									<tr>
-										<td data-th="Product">
-											<div class="row">
-												<div class="col-md-3 text-left">
-													<img src=${item.thumbnailImage} alt="" class="img-fluid d-none d-md-block rounded mb-2 shadow ">
-												</div>
-												<div class="col-md-9 text-left mt-sm-2">
-													<h4>Product Name</h4>
-													<p class="font-weight-light">${item.name}</p>
-												</div>
-											</div>
-										</td>
-										<td data-th="Price">$${item.salePrice}</td>
-										<td data-th="Quantity">
-											<input type="number" class="form-control form-control-lg text-center" value=${item.qty}>
-										</td>
-										<td class="actions" data-th="">
-										</td>
-									</tr>
-
-									`
-								))
-							}
-
-							</tbody>
-						</table>
-						<div class="float-right text-right">
-							<h4>Subtotal:</h4>
-							<h1>$${subtotalPrice()}</h1>
-						</div>
-					</div>
-				</div>
-				<div class="row mt-4 d-flex align-items-center">
-				
-					<div id="paypal-payment-button">
-					</div>
-
-					<button onclick=me()>aa</button>
-
-				</div>
-			</div>
-			</section>
-
-			
-
-			<script src="https://www.paypal.com/sdk/js?client-id=ATZKBUfrjxQCsY9zvAEqt70hBOd5OPgy7xVgWWK21sjnsfqY433imCvsx0i5rT58VXQhq2ga273BXZNw&disable-funding=credit,card">
-
-			</script>
-			<script>
-				function me(){
-					alert(1)
-				}
-				paypal.Buttons({
-					style : {
-						color: 'blue',
-						shape: 'pill'
-					},
-					createOrder: function (data, actions) {
-						return actions.order.create({
-							purchase_units : [{
-								amount: {
-									value: '0.1'
-								}
-							}]
-						});
-					},
-					onApprove: function (data, actions) {
-						return actions.order.capture().then(function (details) {
-							console.log(details)
-						  //  window.location.replace("http://localhost:63342/tutorial/paypal/success.php")
-						})
-					},
-					onCancel: function (data) {
-						//window.location.replace("http://localhost:63342/tutorial/paypal/Oncancel.php")
-					}
-				}).render('#paypal-payment-button');
-		
-			</script>
-
-			</body>
-		</html>
-
-	`; 
-
-	const injectedJavaScript = ``
-		
-
+	function handleResponse (data) {
+		console.log(data);
+        if (data.title === "success") {
+			alert("Payment is successful.");
+          	setShowModal(false);
+        } else if (data.title === "cancel") {
+			alert("Transaction cancelled.");
+			setShowModal(false);
+        } else {
+            return;
+        }
+    }
 		
 		//const { cartItems, cartItemsIsLoading, selectAll } = this.state;
 		
@@ -196,14 +83,24 @@ function Cart ({route, navigation}) {
                     visible={showModal}
                     onRequestClose={() => setShowModal(false)}
                 >
-                   { <WebView
+
+					<WebView
+                        source={{ uri: "http://192.168.1.2:8000/paypal"}}
+                        onNavigationStateChange={data => handleResponse(data)}
+                        injectedJavaScript={`
+							document.getElementById('total').innerHTML="${subtotalPrice()}";
+							document.getElementById('total_value').value="${subtotalPrice()}"
+						`}
+                    />
+
+                   {/* <WebView
                         javaScriptEnabled={true}
                         style={{ flex: 1}}
                         originWhitelist={['*']}
                         source={{ html: htmlContent}}
                         injectedJavaScript={''}
                         
-                    />}
+				   /> */}
                 </Modal>
 
 					<ScrollView>	
@@ -211,8 +108,8 @@ function Cart ({route, navigation}) {
 							<View key={i} style={{flexDirection: 'row', backgroundColor: '#fff', marginBottom: 2, height: 120}}>
 								<View style={[styles.centerElement, {width: 60}]}>
 									<TouchableOpacity style={[styles.centerElement, {width: 32, height: 32}]} onPress={() => selectHandler(i, item.checked)}>
-										{item.checked == 1? <View><Icon name="checkroom"/></View>:<Text>No</Text>}
-										
+										{item.checked == 1? <View><Text style={{height:25,width:25,borderRadius:10,backgroundColor: '#00CC66'}}></Text></View>:
+										<Text style={{height:25,width:25,borderRadius:10,backgroundColor: '#C0C0C0'}}></Text>}
 									</TouchableOpacity>
 						
 								</View>
@@ -242,15 +139,15 @@ function Cart ({route, navigation}) {
 
 						<View style={{flexDirection: 'row'}}>
 							<View style={{flexDirection: 'row', flexGrow: 1, flexShrink: 1, justifyContent: 'space-between', alignItems: 'center'}}>
-								<Text>Select All</Text>
+								<Text></Text>
 								<View style={{flexDirection: 'row', paddingRight: 20, alignItems: 'center'}}>
 									<Text style={{color: '#8f8f8f'}}>SubTotal: </Text>
 									<Text>${subtotalPrice()}</Text>
 								</View>
 							</View>
 						</View>
-						<View style={{flexDirection: 'row', justifyContent: 'flex-end', height: 32, paddingRight: 20, alignItems: 'center'}}>
-							<TouchableOpacity style={[styles.centerElement, {backgroundColor: '#0faf9a', width: 100, height: 25, borderRadius: 5}]} onPress={()=>checkOut()}>
+						<View style={{flexDirection: 'row', justifyContent: 'flex-end', height: 40, paddingRight: 20, alignItems: 'center'}}>
+							<TouchableOpacity style={[styles.centerElement, {backgroundColor: '#0faf9a', width: 100, height: 30, borderRadius: 5}]} onPress={()=>checkOut()}>
 								<Text style={{color: '#ffffff'}}>Checkout</Text>
 							</TouchableOpacity>
 						</View>
